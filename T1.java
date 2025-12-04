271e1a5e7139d7cc5682bd976dfe678fc6407aba
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-12-04 18:12:57 +0530

    commit

---
+++ T1.java
@@ -0,0 +1,18 @@
+	import java.util.TreeSet;
+
+public class T1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        TreeSet<String> t=new TreeSet<String>();
+        
+        t.add("BLACK ");
+        t.add("GREEN ");
+        t.add("ORANGE ");
+        t.add("RED ");
+        t.add("WHITE ");
+        
+        System.out.println("TREESET : "+t);
+	}
+
+}
