c4d344267020b9c2fa5d9173ed19235c015fc51d
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-12-08 18:13:52 +0530

    commit

---
+++ T7.java
@@ -0,0 +1,19 @@
+import java.util.TreeSet;
+
+public class T7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    TreeSet<String> t2=new TreeSet<String>();
+        
+        t2.add("BLUE ");
+        t2.add("YELLOW ");
+        t2.add("GREY ");
+        t2.add("VOILET ");
+        t2.add("COFFEE ");
+        
+        System.out.println("TREESET 2 : "+t2);
+        System.out.println("size of TreeSet : "+t2.size());
+	}
+
+}
