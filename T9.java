ca5bf45059544b58b221af6c59645f001c616347
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-12-11 16:43:38 +0530

    commit

---
+++ T9.java
@@ -0,0 +1,22 @@
+import java.util.TreeSet;
+
+public class T9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     TreeSet<Integer> t1=new TreeSet<Integer>();
+     
+     t1.add(2);
+     t1.add(1);
+     t1.add(3);
+   
+     t1.add(5);
+     t1.add(6);
+    
+     for(int a: t1){
+    	 System.out.println(a);
+     }
+     
+	}
+
+}
